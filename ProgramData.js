94a8f3782e16bf23b20811d8f903e4857f51cefb
phara-0h <menes.alexis@gmail.com>
2025-10-15 16:14:33 -0400

    Create ProgramData.js

---
+++ ProgramData.js
@@ -0,0 +1,156 @@
+export const defaultProgram = {
+  day1: {
+    name: "Lower (Squat)",
+    exercises: [
+      { 
+        name: "Back Squat", 
+        type: "main", 
+        rotation: ["4x4 @ RPE 8-8.5", "4x8 @ RPE 8", "5x3 @ RPE 7-7.5"] 
+      },
+      { 
+        name: "Romanian Deadlift", 
+        type: "secondary", 
+        sets: "4x8-10", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Leg Press", 
+        type: "accessory", 
+        sets: "3x12-15", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Lying Leg Curl", 
+        type: "accessory", 
+        sets: "3x10-12", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Standing Calf Raise", 
+        type: "accessory", 
+        sets: "4x15-20", 
+        rpe: "RPE 7-8" 
+      }
+    ]
+  },
+  day2: {
+    name: "Upper (Horizontal)",
+    exercises: [
+      { 
+        name: "Barbell Bench Press", 
+        type: "main", 
+        rotation: ["4x4 @ RPE 8-8.5", "4x8 @ RPE 8", "5x3 @ RPE 7-7.5"] 
+      },
+      { 
+        name: "Chest Row Machine", 
+        type: "secondary", 
+        sets: "4x8-10", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Plate-Loaded Incline Press", 
+        type: "secondary", 
+        sets: "3x10-12", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Single-Arm DB Row", 
+        type: "secondary", 
+        sets: "3x10-12/arm", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Cable Chest Fly", 
+        type: "accessory", 
+        sets: "3x12-15", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Face Pulls", 
+        type: "accessory", 
+        sets: "3x15-20", 
+        rpe: "RPE 7-8" 
+      }
+    ]
+  },
+  day3: {
+    name: "Lower (Deadlift)",
+    exercises: [
+      { 
+        name: "Conventional Deadlift", 
+        type: "main", 
+        rotation: ["4x4 @ RPE 8-8.5", "5x3 @ RPE 7-7.5"] 
+      },
+      { 
+        name: "Front Squat", 
+        type: "secondary", 
+        sets: "3x8-10", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Walking Lunges", 
+        type: "accessory", 
+        sets: "3x10/leg", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Leg Extension", 
+        type: "accessory", 
+        sets: "3x12-15", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Seated Calf Raise", 
+        type: "accessory", 
+        sets: "4x15-20", 
+        rpe: "RPE 7-8" 
+      }
+    ]
+  },
+  day4: {
+    name: "Upper (Vertical + Arms)",
+    exercises: [
+      { 
+        name: "Seated Barbell OHP", 
+        type: "main", 
+        rotation: ["4x4 @ RPE 8-8.5", "4x8 @ RPE 8", "5x3 @ RPE 7-7.5"] 
+      },
+      { 
+        name: "Pull-Ups/Lat Pulldown", 
+        type: "secondary", 
+        sets: "4x6-8", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "DB Lateral Raise", 
+        type: "accessory", 
+        sets: "3x12-15", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Barbell Curl", 
+        type: "accessory", 
+        sets: "3x8-10", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Rope Pushdown", 
+        type: "accessory", 
+        sets: "3x10-12", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Hammer Curl", 
+        type: "accessory", 
+        sets: "3x10-12", 
+        rpe: "RPE 7-8" 
+      },
+      { 
+        name: "Overhead Tricep Ext", 
+        type: "accessory", 
+        sets: "3x10-12", 
+        rpe: "RPE 7-8" 
+      }
+    ]
+  }
+};
